41528f2dbbf3cbcc6a112788522695d1a70b1d87
ntoulemont <n.toulemont@gmail.com>
2022-10-16 18:37:53 +0200

    fix: dbs packages dev mode issue

--- packages/auth_database/src/index.ts
+++ packages/auth_database/src/index.ts
@@ -2,14 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 export * from '@prisma/client'
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient
-    }
-  }
-}
-
 let prisma: PrismaClient
 
 if (typeof window === 'undefined') {
