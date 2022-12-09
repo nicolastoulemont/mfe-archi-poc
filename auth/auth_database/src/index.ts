52bcc68ff4b8d151c318aa6726db051e9eb6d9df
ntoulemont <n.toulemont@gmail.com>
2022-12-09 17:57:02 +0100

    feat: no build errors

--- auth/auth_database/src/index.ts
+++ auth/auth_database/src/index.ts
@@ -2,18 +2,4 @@ import { PrismaClient } from '@prisma/client'
 
 export * from '@prisma/client'
 
-let prisma: PrismaClient
-
-if (typeof window === 'undefined') {
-  if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient()
-    }
-
-    prisma = global.prisma
-  }
-}
-
-export { prisma }
+export const prisma = new PrismaClient()
